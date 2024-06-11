10eb85ab5e5679a9ec94415a4768498c0e07029a
carstendold1@gmail.com <88604752+OriginalUncompiled@users.noreply.github.com>
2024-06-11 17:12:42 +0200

    PROPERLY added supabase connection

--- server.js
+++ server.js
@@ -9,30 +9,23 @@ import handleProfile from './controllers/profile.js';
 import handleSignIn from './controllers/signin.js';
 import { handleImage, handleApiCall } from './controllers/image.js';
 
-// const supabaseUrl = 'https://yxrcjejgusfnklhyewbl.supabase.co';
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4cmNqZWpndXNmbmtsaHlld2JsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxMTQxNDYsImV4cCI6MjAzMzY5MDE0Nn0.-NjOTI3MjhQZmurEGecYvP1dXjV3uxCrQ9FTMrEeogY';
-// const supabaseSecret = 'FETVAxVZhMKi++qq2TrjRoTduFChWxY0dBqhSTLJYyGJY10h8H01it74d8rVBicEfhqTw3efA+ppuSq4cXGDDw==';
+const supabaseUrl = 'https://yxrcjejgusfnklhyewbl.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4cmNqZWpndXNmbmtsaHlld2JsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxMTQxNDYsImV4cCI6MjAzMzY5MDE0Nn0.-NjOTI3MjhQZmurEGecYvP1dXjV3uxCrQ9FTMrEeogY';
+const supabaseSecret = 'FETVAxVZhMKi++qq2TrjRoTduFChWxY0dBqhSTLJYyGJY10h8H01it74d8rVBicEfhqTw3efA+ppuSq4cXGDDw==';
 
-// const supabase = createClient(supabaseUrl, supabaseKey, supabaseSecret);
-
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: 'aws-0-eu-central-1.pooler.supabase.com',
-        database: 'postgres',
-        port: 6543,
-        user: 'postgres.yxrcjejgusfnklhyewbl',
-        password: 'yw35!9NsvT6MBLN'
-    },
-});
+const supabase = createClient(supabaseUrl, supabaseKey, supabaseSecret);
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+const db = knex({
+    client: 'pg',
+    connection: 'postgres://postgres.yxrcjejgusfnklhyewbl:yw35!9NsvT6MBLN@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
+});
 
-app.get('/', (request, response) => { response.send('it\'s working boss')}) //database.users
+app.get('/', (request, response) => { response.send(database.users)})
 app.post('/signin', (request, response) => { handleSignIn(request, response, db, bcrypt) })
 app.post('/register', (request, response) => { handleRegister(request, response, db, bcrypt) })
 app.get('/profile/:id', (request, response) => { handleProfile(request, response, db) })
