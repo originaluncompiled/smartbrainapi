992297a5be3e4d38e0313420f6c74079d08ea08f
carstendold1@gmail.com <88604752+OriginalUncompiled@users.noreply.github.com>
2024-06-11 17:37:34 +0200

    chatgpt please save me

--- controllers/register.js
+++ controllers/register.js
@@ -1,33 +1,68 @@
-const handleRegister = (request, response, supabase, bcrypt) => {
+const handleRegister = async (request, response, supabase, bcrypt) => {
     const { email, name, password } = request.body;
     if (!email || !name || !password) {
         return response.status(400).json('Invalid Details');
     }
+
     let salt = bcrypt.genSaltSync(10);
     let hash = bcrypt.hashSync(password, salt);
-    supabase.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0].email,
-                    name: name,
-                    joined: new Date()
-                })
-                .then(user => {
-                    response.json(user[0]);
-                })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => response.status(400).json('Cannot register account'))
+
+    try {
+        // Insert into 'login' table
+        const { data: loginData, error: loginError } = await supabase
+            .from('login')
+            .insert([{ hash: hash, email: email }])
+            .select('email');
+
+        if (loginError) throw loginError;
+
+        // Insert into 'users' table
+        const { data: userData, error: userError } = await supabase
+            .from('users')
+            .insert([{ email: loginData[0].email, name: name, joined: new Date() }])
+            .select('*');
+
+        if (userError) throw userError;
+
+        response.json(userData[0]);
+    } catch (error) {
+        response.status(400).json('Cannot register account');
+    }
 }
 
 export default handleRegister;
+
+
+// const handleRegister = (request, response, supabase, bcrypt) => {
+//     const { email, name, password } = request.body;
+//     if (!email || !name || !password) {
+//         return response.status(400).json('Invalid Details');
+//     }
+//     let salt = bcrypt.genSaltSync(10);
+//     let hash = bcrypt.hashSync(password, salt);
+//     supabase.transaction(trx => {
+//         trx.insert({
+//             hash: hash,
+//             email: email
+//         })
+//         .into('login')
+//         .returning('email')
+//         .then(loginEmail => {
+//             return trx('users')
+//                 .returning('*')
+//                 .insert({
+//                     email: loginEmail[0].email,
+//                     name: name,
+//                     joined: new Date()
+//                 })
+//                 .then(user => {
+//                     response.json(user[0]);
+//                 })
+//         })
+//         .then(trx.commit)
+//         .catch(trx.rollback)
+//     })
+//     .catch(err => response.status(400).json('Cannot register account'))
+// }
+
+// export default handleRegister;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import express from 'express';
 import bcrypt from 'bcrypt';
 import cors from 'cors';
-import knex from 'knex';
+// import knex from 'knex';
 
 import handleRegister from './controllers/register.js';
 import handleProfile from './controllers/profile.js';
@@ -11,9 +11,8 @@ import { handleImage, handleApiCall } from './controllers/image.js';
 
 const supabaseUrl = 'https://yxrcjejgusfnklhyewbl.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4cmNqZWpndXNmbmtsaHlld2JsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxMTQxNDYsImV4cCI6MjAzMzY5MDE0Nn0.-NjOTI3MjhQZmurEGecYvP1dXjV3uxCrQ9FTMrEeogY';
-const supabaseSecret = 'FETVAxVZhMKi++qq2TrjRoTduFChWxY0dBqhSTLJYyGJY10h8H01it74d8rVBicEfhqTw3efA+ppuSq4cXGDDw==';
 
-const supabase = createClient(supabaseUrl, supabaseKey, supabaseSecret);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 
